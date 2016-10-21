263fe2f98d2c762c58b70f62ac22f9ec71d6fd82
陈旭 <897939354@qq.com>
2016-10-21 13:47:22 +0800

    commit

---
+++ GitManage/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitManage
+//
+//  Created by 陈旭 on 2016/10/21.
+//  Copyright © 2016年 陈旭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("111")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
