9863382f5b448ab6bcb1840d97ed74768c74d303
陈旭 <897939354@qq.com>
2016-10-21 13:56:22 +0800

    增加print

--- GitManage/ViewController.swift
+++ GitManage/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("111222")
-        
+         print("111222")
     }
 
     override func didReceiveMemoryWarning() {
